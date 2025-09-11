ab91bd05eeb29ef26ff9a4cfd449d914066a279b
OsirisOR <osirisojedar424@gmail.com>
2025-09-11 03:10:12 -0600

    Ejercicio 1

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -2,23 +2,29 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Práctica 3</title>
+    <title>Práctica 5</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
     <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
     <?php
-        //AQUI VA MI CÓDIGO PHP
-        $_myvar;
-        $_7var;
-        //myvar;       // Inválida
-        $myvar;
-        $var7;
-        $_element1;
-        //$house*5;     // Invalida
+     
+        // Variables válidas para demostración
+        $_myvar    = 'valor1';
+        $_7var     = 'valor2';
+        $myvar     = 'valor3';
+        $var7      = 'valor4';
+        $_element1 = 'valor5';
         
         echo '<h4>Respuesta:</h4>';   
+        echo '<pre>';
+        var_dump($_myvar);
+        var_dump($_7var);
+        var_dump($myvar);
+        var_dump($var7);
+        var_dump($_element1);
+        echo '</pre>';
     
         echo '<ul>';
         echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
@@ -29,6 +35,8 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+
+        unset($_myvar, $_7var, $myvar, $var7, $_element1);
     ?>
 </body>
 </html>
