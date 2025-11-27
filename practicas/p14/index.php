61d7727a238dd0be05a2ae4fadace9b718bdb9cd
OsirisOR <osirisojedar424@gmail.com>
2025-11-26 22:38:02 -0600

    Ejercicio 1

---
+++ practicas/p14/index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/', function ( $request, $response, $args ) {
+    $response->write("Hola Mundo Slim!!!");
+    return $response;
+});
+
+$app->run();
+?>
